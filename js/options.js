f3af97bfc9c75eec605e99cc00aa82a40adf96b0
fuyuton <fuyuton@hr.nazo.cc>
2019-12-29 22:31:31 +0900

    Revert "Revert "改行動作のbug fix""
    This reverts commit b1ae82f8f42727a99f0ac754ed44137d77faabdb.

--- js/options.js
+++ js/options.js
@@ -5,17 +5,53 @@ $(function(){
 	// 設定ウィンドウを閉じる
 	$("#save").click(function () {
 		localStorage["link_format"] = $("#link_format").val();
-		localStorage["new_line"] = $("#new_line").val();
+		
+		if ($("#new_line").prop("checked")) {
+			localStorage["new_line"] = $("#new_line").val();
+		}else{
+			localStorage["new_line"] = 0;
+
+		}
+		//localStorage["new_line"] = $("#new_line").val();
+		
+		//chrome.storage.local.set({'link_format': #("#link_format").val(), 'new_line':  $("#new_line").val()}, function () {
+		//});
+		
 		window.close();
 	});
 
 	// オプション画面の初期値を設定する
+	// chrome.storage を利用したchromeへのデータ保存 - Qiita https://qiita.com/shimutaya/items/e8835d6ce794ef6c73cf
+
 	if (localStorage["link_format"]) {
 		$("#link_format").val(localStorage["link_format"]);
+		
+		//chrome.storage.local.get("link_format", function (value) {
+		//	$("#link_format").val(value.key);
+		//});
+	}else{
+		//storageにlink_formatがなければ
+		//link_format = "Text"
+		$("#link_format").val("Text");
+		//localStorageに保存
+		localStorage["link_format"] = $("#link_format").val();
+
 	}
+	
 	if (localStorage["new_line"]) {
-		var new_line = localStorage["new_line"];
-		$("#new_line option[value=" + new_line + "]").attr("selected", true);
+		
+		if (localStorage["new_line"] == 1) {
+			$("#new_line").prop("checked", true);
+		}else{
+			$("#new_line").prop("checked", false);
+		}
+	}else{
+		//localstorageにnew_lineがなければ
+		//初期値: checked
+		$("#new_line").prop("checked", true);
+		//localStorageに保存
+		localStorage["new_line"] = $("#new_line").val();
+
 	}
 
 });
